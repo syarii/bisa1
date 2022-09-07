e6ba3db4cdf32c3cadf364bbc172b6e27589d90b
syarii <kiostrisifak35@gmail.com>
2022-09-08 01:02:18 +0700

    bisa1

---
+++ settings.js
@@ -0,0 +1,42 @@
+/**
+   * Made By Fandyyy 🕴️
+   * Subscribe FBOTZ YT
+   * Follow https://instagram.com/_nzrlafndi
+   * Follow https://github.com/FBOTZ-YT
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Ubah Disini
+global.ownerNumber = "085759681568","087819503464",
+global.ownerName = "Xzyyo",
+global.packname = "xzyyobot",
+global.author = "Whatsapp Bot 2022",
+global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
+global.sessionName = 'sesi'
+
+//Message Nya Ubah Disini
+global.mess = {
+admin: 'You Are Not Admin!',
+botAdmin: 'Bot Not Admin!',
+botOwner: 'You Are Not My Owner!',
+group: 'Only Group!',
+private: 'Only Private Chat',
+wait: 'Loading...',
+done: 'Done!'
+}
+
+
+//Sesuaikan
+global.thumb = fs.readFileSync('./media/logo.jpeg')
+global.donasi = fs.readFileSync('./media/donasii.jpg')
+global.thumbnail = fs.readFileSync('./media/ichi.mp4')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
